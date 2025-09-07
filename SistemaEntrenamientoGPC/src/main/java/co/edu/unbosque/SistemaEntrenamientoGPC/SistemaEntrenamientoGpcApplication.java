be0933a004553e4b42ef8d810d7b100bf3ac9a1e
LuisVivasC <lc.vivascruz@gmail.com>
2025-09-07 00:25:56 -0500

    Creacion Front-1 Biblioteca Artemisa

---
+++ SistemaEntrenamientoGPC/src/main/java/co/edu/unbosque/SistemaEntrenamientoGPC/SistemaEntrenamientoGpcApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.SistemaEntrenamientoGPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEntrenamientoGpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEntrenamientoGpcApplication.class, args);
+	}
+
+}
---
+++ SistemaEntrenamientoGPC/src/test/java/co/edu/unbosque/SistemaEntrenamientoGPC/SistemaEntrenamientoGpcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.SistemaEntrenamientoGPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEntrenamientoGpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
