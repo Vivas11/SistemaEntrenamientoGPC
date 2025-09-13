395fa5885c0e090671d2bfbe5fbe93fd42d5351a
LuisVivasC <lc.vivascruz@gmail.com>
2025-09-13 18:00:44 -0500

    Creacion Back

---
+++ sistemaentrenamientoGPC_back/src/main/java/co/edu/unbosque/sistemaentrenamientoGPC_back/SistemaentrenamientoGpcBackApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.sistemaentrenamientoGPC_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaentrenamientoGpcBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaentrenamientoGpcBackApplication.class, args);
+	}
+
+}
---
+++ sistemaentrenamientoGPC_back/src/test/java/co/edu/unbosque/sistemaentrenamientoGPC_back/SistemaentrenamientoGpcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.sistemaentrenamientoGPC_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaentrenamientoGpcBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
