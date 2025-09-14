4292af6f982856028bf99c6fbb5393f774c7d785
dylanbmk <dylancho0210@gmail.com>
2025-09-14 16:02:28 -0500

    correccion admin

--- sistemaentrenamientoGPC_back/src/main/java/co/edu/unbosque/sistemaentrenamientoGPC_back/entity/Admin.java
+++ sistemaentrenamientoGPC_back/src/main/java/co/edu/unbosque/sistemaentrenamientoGPC_back/entity/Admin.java
@@ -1,71 +1,25 @@
 package co.edu.unbosque.sistemaentrenamientoGPC_back.entity;
 
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 
 @Entity
 
 public class Admin extends Usuario{
-	
-	
-	private String tipoAdmin;
-	
-	
-	public Admin() {
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public Admin(String tipoAdmin) {
-		super();
-		this.tipoAdmin = tipoAdmin;
-	}
-
 
 	public Admin(String nombre, String correo, int edad) {
 		super(nombre, correo, edad);
+	}
+
+	public Admin() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(tipoAdmin);
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Admin other = (Admin) obj;
-		return Objects.equals(tipoAdmin, other.tipoAdmin);
-	}
-
-
-	public String getTipoAdmin() {
-		return tipoAdmin;
-	}
-
-
-	public void setTipoAdmin(String tipoAdmin) {
-		this.tipoAdmin = tipoAdmin;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Admin [tipoAdmin=" + tipoAdmin + "]";
-	}
+	
+	
 	
 	
 	
