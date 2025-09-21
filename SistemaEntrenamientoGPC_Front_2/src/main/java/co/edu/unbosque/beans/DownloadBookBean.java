8262501e3b7f74a5d212680c107ba7b46f498cbb
kevx2705 <kevinalexandertunare2007@gmail.com>
2025-09-20 18:17:37 -0600

    enlaces book completos

--- SistemaEntrenamientoGPC_Front_2/src/main/java/co/edu/unbosque/beans/DownloadBookBean.java
+++ SistemaEntrenamientoGPC_Front_2/src/main/java/co/edu/unbosque/beans/DownloadBookBean.java
@@ -139,43 +139,160 @@ public class DownloadBookBean {
 //	Bit Magic (Ingles)
 
 	public StreamedContent getBitMagic() {
-		return buildFile("Bit Magic.pdf", "");
+		return buildFile("Bit Magic.pdf", "https://biblioteca-artemisa.netlify.app/assets/pdfs/Bit-Magic.pdf");
 	}
 
 //	Advanced Data Structure (ingles)
 
 	public StreamedContent getAdvancedDataStructure() {
-		return buildFile("Advanced Data Structure.pdf", "");
+		return buildFile("Advanced Data Structure.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Advanced-Data-Structure.pdf");
 	}
 
 //	Arrays (Ingles)
 
 	public StreamedContent getArrays() {
-		return buildFile("Arrays.pdf", "");
+		return buildFile("Arrays.pdf", "https://biblioteca-artemisa.netlify.app/assets/pdfs/Arrays.pdf");
 	}
 
 //	Binary Search Tree (Ingles)
 
 	public StreamedContent getBinarySearchTree() {
-		return buildFile("Binary Search Tree.pdf", "");
+		return buildFile("Binary Search Tree.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Binary-Search-Tree.pdf");
 	}
 
 //Pilas, Colas, Listas, Árboles y Grafos
-	
+
 	public StreamedContent getPilasColasListasÁrbolesGrafos() {
-		return buildFile("Pilas, Colas, Listas, Árboles y Grafos.pdf", "");
+		return buildFile("Pilas, Colas, Listas, Árboles y Grafos.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/estructuras%20de%20datos.pdf");
 	}
 
 //	Linked List (Ingles)
-	
-	
+
 	public StreamedContent getLinkedList() {
-		return buildFile("Pilas, Colas, Listas, Árboles y Grafos.pdf", "");
+		return buildFile("Pilas, Colas, Listas, Árboles y Grafos.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Linked-List.pdf");
 	}
 
-//	Linked List
-	
-	
+//	Principles of Algorithmic Problem Solving (Ingles)
+
+	public StreamedContent getPrinciplesAlgorithmicProblemSolving() {
+		return buildFile("Principles of Algorithmic Problem Solving.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Matrix.pdf");
+	}
+
+//	Competitive programming handbook
+
+	public StreamedContent getCompetitiveProgrammingHandbook() {
+		return buildFile("Competitive programming handbook.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/(Espa%C3%B1ol)%20Competitive%20Programmer%E2%80%99s%20Handbook.pdf");
+	}
+
+//	Geometric Algorithms (ingles)
+
+	public StreamedContent getGeometricAlgorithms() {
+		return buildFile("Geometric Algorithms.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Geometric-Algorithms.pdf");
+	}
+
+//Graphs (ingles)
+
+	public StreamedContent getGraphs() {
+		return buildFile("Graphs.pdf", "https://biblioteca-artemisa.netlify.app/assets/pdfs/Graphs.pdf");
+	}
+
+//Guía de programación en Java
+
+	public StreamedContent getGuíaprogramaciónJava() {
+		return buildFile("Guía de programación en Java.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Intro%20java.pdf");
+	}
+
+//Guia de programación en C++
+
+	public StreamedContent getGuiaprogramaciónC() {
+		return buildFile("Guia de programación en C++.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/intro%20cpp.pdf");
+	}
+
+//Guia de programación en Python
+
+	public StreamedContent getGuiaProgramaciónPython() {
+		return buildFile("Guia de programación en Python.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/intro%20py.pdf");
+	}
+
+//	C++ (Ingles)
+
+	public StreamedContent getC() {
+		return buildFile("C++.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/CPlusPlusNotesForProfessionals.pdf");
+	}
+
+//	Java (Ingles)
+
+	public StreamedContent getJava() {
+		return buildFile("Java.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/JavaNotesForProfessionals.pdf");
+	}
+
+//	Python (Ingles)
+
+	public StreamedContent getPython() {
+		return buildFile("Python.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/PythonNotesForProfessionals.pdf");
+	}
+
+//	Ruby (Ingles)
+
+	public StreamedContent getRuby() {
+		return buildFile("Ruby.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/RubyNotesForProfessionals.pdf");
+	}
+
+//	Discrete Structures (ingles)
+
+	public StreamedContent getDiscreteStructures() {
+		return buildFile("Discrete Structures.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Matematica%20discreta.pdf");
+	}
+
+//	Matemática discreta
+
+	public StreamedContent getMatemáticaDiscreta() {
+		return buildFile("Matemática discreta.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/matemtica_discreta_para_informticos.pdf");
+	}
+
+//	Principles of Algorithmic Problem Solving (ingles)
+
+	public StreamedContent getPrinciplesAlgorithmicProblemSolvingIngles() {
+		return buildFile("Principles of Algorithmic Problem Solving (ingles).pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Principles%20of%20Algorithmic%20Problem%20Solving.pdf");
+	}
+
+//	Combinatorial (Ingles)
+
+	public StreamedContent getCombinatorial() {
+		return buildFile("Combinatorial.pdf", "https://biblioteca-artemisa.netlify.app/assets/pdfs/Combinatorial.pdf");
+	}
+
+//	Discrete Structures (Ingles)
+
+	public StreamedContent getDiscreteStructuresIngles() {
+		return buildFile("Discrete Structures.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/Matematica%20discreta.pdf");
+	}
+
+//	Matemática discreta
+
+	public StreamedContent getMatemáticaDiscreta2() {
+		return buildFile("Matemática discreta.pdf",
+				"https://biblioteca-artemisa.netlify.app/assets/pdfs/matemtica_discreta_para_informticos.pdf");
+	}
+
 	private StreamedContent buildFile(String name, String url) {
 		return DefaultStreamedContent.builder().name(name).contentType("application/pdf").stream(() -> {
 			try {
